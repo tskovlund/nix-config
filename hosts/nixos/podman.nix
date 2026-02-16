0ae6b7d551b82f0b5450bf3f03993a3ae0e74357
Thomas Skovlund Hansen <thomas@skovlund.dev>
2026-02-16 13:40:54 +0000

    docs(nixos): add comments to podman module

--- hosts/nixos/podman.nix
+++ hosts/nixos/podman.nix
@@ -1,6 +1,8 @@
 { ... }:
 
 {
+  # Daemonless container runtime. dockerCompat provides `docker` CLI alias
+  # and socket, needed for tools like Testcontainers that expect Docker.
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
